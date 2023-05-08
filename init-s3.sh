1405c9b0f2cb058513f25a283b279fd1da7a597c
Oleksandr <131167863+AnykeySW88@users.noreply.github.com>
2023-05-08 09:53:28 +0300

    Update init-s3.sh

--- init-s3.sh
+++ init-s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a small text file
-echo "Hello, world!" > myfile.txt
+echo "Get ready for the test!" > myfile.txt
 
 # Create an S3 bucket
 aws s3api create-bucket --bucket bucket-for-leagrnin --region us-west-2
